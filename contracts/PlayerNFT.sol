8835687e98edf1f07827d1c77b3cde9d22c4d433
Sebas <sheva3232@gmail.com>
2023-05-08 21:45:59 -0500

    v 0,1 smart contract

--- contracts/PlayerNFT.sol
+++ contracts/PlayerNFT.sol
@@ -8,10 +8,20 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+
+// PUSH Comm Contract Interface
+interface IPUSHCommInterface {
+    function sendNotification(address _channel, address _recipient, bytes calldata _identity) external;
+}
+
 contract FU3 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+
+    // EPNS COMM ADDRESS ON ETHEREUM goerli, CHECK THIS: https://docs.epns.io/developers/developer-tooling/epns-smart-contracts/epns-contract-addresses
+    address public EPNS_COMM_ADDRESS = 0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa;
+
 
     constructor() ERC721("FU3", "FU3") {}
 
@@ -29,13 +39,12 @@ contract FU3 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable {
         uint8 kick;
         uint8 positioning;
     }   
-    //mappint PlayerNFT ID with player properties
+    //mapping PlayerNFT ID with player properties
+    
     mapping (uint256 => Player) private playerInfo;
-    mapping (uint256 => bool) private allowed;
     function pause() public onlyOwner {
         _pause();
     }
-
     function unpause() public onlyOwner {
         _unpause();
     }
@@ -75,17 +84,38 @@ contract FU3 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable {
     ) public view override(ERC721, ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
+
+
     //Player's Functions
     function readProperties (uint256 _tokenId) external view returns (Player memory) {
         require(_isApprovedOrOwner(_msgSender(), _tokenId), "Caller must be the Token owner");
         return playerInfo[_tokenId];
     } 
-    //Functions called by Owner to approve modify player stats
+
+    //Functions called by Owner to modify player stats
     function editPlayerStats (uint256 _tokenId, Player memory _player, address _sender) public onlyOwner{
         modifyPlayerStats(_tokenId, _player, _sender);
     }
     function modifyPlayerStats (uint256 _tokenId, Player memory _player, address _sender) internal {
-        require(_isApprovedOrOwner(_sender, _tokenId), "Caller must be the Token owner");
+        require(_isApprovedOrOwner(_sender, _tokenId), "Function Caller must be the Token owner");
         playerInfo[_tokenId] = _player;
+        IPUSHCommInterface(EPNS_COMM_ADDRESS).sendNotification(
+            0x4EEe90A694935018609190490DF345b283897df4, // from channel
+            _sender, // to recipient, put address(this) in case you want Broadcast or Subset. For Targetted put the address to which you want to send
+            bytes(
+                string(
+                    // We are passing identity here: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                    abi.encodePacked(
+                        "0", // this is notification identity: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                        "+", // segregator
+                        "3", // this is payload type: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/payload (1, 3 or 4) = (Broadcast, targetted or subset)
+                        "+", // segregator
+                        "Player has been improved !", // this is notificaiton title
+                        "+", // segregator
+                        "Your NFT received an upgrade of properties ! " // notification body
+                    )
+                )
+            )
+        );
     }
 }

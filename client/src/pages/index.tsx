a38df2cc1a7093d5903cbbb8060d20fbc3b6dd9e
Sebas <sheva3232@gmail.com>
2023-05-16 09:13:31 -0500

    implementing Player NFT

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,23 +1,63 @@
-import Notificacions from '@/components/notifications';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { useEffect } from 'react';
-import { useAccount } from 'wagmi';
+import Notificacions from "@/components/notifications";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useEffect } from "react";
+import { useAccount } from "wagmi";
+import { ethers } from "ethers";
+import web3 from './web3.min.js'
 
 export default function Home() {
-  const {address, isConnected} = useAccount();
+  const { address, isConnected } = useAccount();
   const env = process.env.PUSH_ENV;
-
+  let firma;
   useEffect(() => {
     console.log(address);
+  }, [isConnected]);
+  const sign = async () => {
+    let tokenId = 1;
+    let to = '0xe7911A3B3647B4723320502B25B21aEe78eAfeb0';
+    let player = '001002003004005006007008009010011012';
+    let hash = await ethers.utils.solidityKeccak256(
+      ["uint256", "address", "string"],
+      [tokenId, to, player]
+    );
+    console.log("hash= " + hash); // 0x8b939d18a1d2a152f82a5edb0af94e6b4bdb4b959f6f4863e73fa77931519371
+
+    firma = await signMessage(hash, address)
+    .then((signature) => {
+      console.log("Firma:", signature);
+      return signature;
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
     
-  }, [isConnected])
+    console.log("firma= " + firma); // 
+  };
+  async function signMessage(hash, address) {
+    // Conectar al proveedor de la billetera Injected Web3
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
   
+    // Solicitar al usuario que apruebe la conexión y acceda a su cuenta
+    await provider.send("eth_requestAccounts", []);
+  
+    // Obtener el objeto Signer de la dirección especificada
+    const signer = provider.getSigner(address);
+  
+    // Firmar el mensaje
+    const signature = await signer.signMessage(hash);
+  
+    return signature;
+  }
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
       <div className="relative flex flex-col place-items-center gap-8 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[200px] after:w-[260px] after:translate-x-1/3 after:bg-gradient-conic   after:blur-2xl after:content-[''] before:from-transparent before:to-blue-700/10 after:from-sky-900 after:via-[#0141ff]/40 before:lg:h-[360px]">
-        <p className="text-3xl font-bold tracking-wider">{'<Ready to code />'}</p>
+        <p className="text-3xl font-bold tracking-wider">
+          {"<Ready to code />"}
+        </p>
         <ConnectButton />
-        <Notificacions/>
+        <Notificacions />
+
+        <button onClick={sign}>Sign</button>
       </div>
     </main>
   );
--- contracts/PlayerNFT.sol
+++ contracts/PlayerNFT.sol
@@ -10,28 +10,18 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 // PUSH Comm Contract Interface
 interface IPUSHCommInterface {
-    function sendNotification(address _channel, address _recipient, bytes calldata _identity) external;
+    function sendNotification(
+        address _channel,
+        address _recipient,
+        bytes calldata _identity
+    ) external;
 }
 
 contract FU3 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable {
     using Counters for Counters.Counter;
-    struct Player {
-        uint8 ritm;
-        uint8 shoot;
-        uint8 pass;
-        uint8 dodge;
-        uint8 defense;
-        uint8 physic;
-        uint8 stretch;
-        uint8 stop;
-        uint8 reflex;
-        uint8 keeperSpeed;
-        uint8 kick;
-        uint8 positioning;
-    }  
     string[10] uris = [
-        "https://gateway.pinata.cloud/ipfs/QmPdEwjNdPr9yvommNmy87CvdoDiB9Kr3jpKGZVJM9XfjC/1.json", 
-        "https://gateway.pinata.cloud/ipfs/QmPdEwjNdPr9yvommNmy87CvdoDiB9Kr3jpKGZVJM9XfjC/2.json", 
+        "https://gateway.pinata.cloud/ipfs/QmPdEwjNdPr9yvommNmy87CvdoDiB9Kr3jpKGZVJM9XfjC/1.json",
+        "https://gateway.pinata.cloud/ipfs/QmPdEwjNdPr9yvommNmy87CvdoDiB9Kr3jpKGZVJM9XfjC/2.json",
         "https://gateway.pinata.cloud/ipfs/QmPdEwjNdPr9yvommNmy87CvdoDiB9Kr3jpKGZVJM9XfjC/3.json",
         "https://gateway.pinata.cloud/ipfs/QmPdEwjNdPr9yvommNmy87CvdoDiB9Kr3jpKGZVJM9XfjC/4.json",
         "https://gateway.pinata.cloud/ipfs/QmPdEwjNdPr9yvommNmy87CvdoDiB9Kr3jpKGZVJM9XfjC/5.json",
@@ -40,19 +30,22 @@ contract FU3 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable {
         "https://gateway.pinata.cloud/ipfs/QmPdEwjNdPr9yvommNmy87CvdoDiB9Kr3jpKGZVJM9XfjC/8.json",
         "https://gateway.pinata.cloud/ipfs/QmPdEwjNdPr9yvommNmy87CvdoDiB9Kr3jpKGZVJM9XfjC/9.json",
         "https://gateway.pinata.cloud/ipfs/QmPdEwjNdPr9yvommNmy87CvdoDiB9Kr3jpKGZVJM9XfjC/10.json"
-    ]; 
+    ];
     Counters.Counter private _tokenIdCounter;
     address private EPNS_COMM_ADDRESS;
     address payable admin;
     uint256 public tid;
     uint256 mintPrice;
-    mapping (uint256 => Player) private playerInfo;
+    string initialProps = "001001001001001001001001001001001001";
+    //ritm,shoot,pass,dodge,defense,physic,stretch,stop,reflex,keeperSpeed,kick,positioning
+    mapping(uint256 => string) private playerInfo;
 
     constructor() ERC721("FU3", "FU3") {
         EPNS_COMM_ADDRESS = 0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa;
-        mintPrice = 0.0001 ether;
+        mintPrice = 0.00001 ether;
         admin = payable(msg.sender);
     }
+
     function pause() public onlyOwner {
         _pause();
     }
@@ -61,16 +54,24 @@ contract FU3 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable {
         _unpause();
     }
 
-    function mint (uint8 _avatarIndex, Player calldata _player) external payable {
-        require (msg.value == mintPrice, "Price must be the asking one");
-        (bool success, ) = admin.call{ value: msg.value }("");
+    function mint(uint8 _avatarIndex)
+        external
+        payable
+    {
+        require(msg.value == mintPrice, "Price must be the asking one");
+        (bool success, ) = admin.call{value: msg.value}("");
         require(success, "Payment did not proceed");
-        safeMint(msg.sender, uris[_avatarIndex], _player);
+        safeMint(msg.sender, uris[_avatarIndex], initialProps);
     }
-    function safeMint(address to, string memory uri, Player calldata _player) internal {
+
+    function safeMint(
+        address _to,
+        string memory uri,
+        string memory _player
+    ) internal {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
+        _safeMint(_to, tokenId);
         _setTokenURI(tokenId, uri);
         playerInfo[tokenId] = _player;
     }
@@ -86,54 +87,129 @@ contract FU3 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable {
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(
-        uint256 tokenId
-    ) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
         super._burn(tokenId);
     }
 
-    function tokenURI(
-        uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
         return super.tokenURI(tokenId);
     }
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 
-
     //Player's Functions
-    function readProperties (uint256 _tokenId) external view returns (Player memory) {
-        require(_isApprovedOrOwner(_msgSender(), _tokenId), "Caller must be the Token owner");
+    function readProperties(uint256 _tokenId)
+        external
+        view
+        returns (string memory)
+    {
+        require(
+            _isApprovedOrOwner(_msgSender(), _tokenId),
+            "Caller must be the Token owner"
+        );
         return playerInfo[_tokenId];
-    } 
+    }
 
     //Functions called by Owner to modify player stats
-    function editPlayerStats (uint256 _tokenId, Player memory _player, address _sender) public onlyOwner{
-        modifyPlayerStats(_tokenId, _player, _sender);
+    function editPlayerStats(
+        uint256 _tokenId,
+        string memory _player,
+        bytes memory _signature
+    ) external whitelisted(msg.sender, _tokenId, _player, _signature) {
+        modifyPlayerStats(_tokenId, _player, msg.sender);
     }
-    function modifyPlayerStats (uint256 _tokenId, Player memory _player, address _sender) internal {
-        require(_isApprovedOrOwner(_sender, _tokenId), "Function Caller must be the Token owner");
-        playerInfo[_tokenId] = _player;
-        IPUSHCommInterface(EPNS_COMM_ADDRESS).sendNotification(
-            0x4EEe90A694935018609190490DF345b283897df4, // from channel
-            _sender, // to recipient, put address(this) in case you want Broadcast or Subset. For Targetted put the address to which you want to send
-            bytes(
-                string(
-                    // We are passing identity here: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
-                    abi.encodePacked(
-                        "0", // this is notification identity: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
-                        "+", // segregator
-                        "3", // this is payload type: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/payload (1, 3 or 4) = (Broadcast, targetted or subset)
-                        "+", // segregator
-                        "Player has been improved !", // this is notificaiton title
-                        "+", // segregator
-                        "Your NFT received an upgrade of properties ! " // notification body
-                    )
-                )
-            )
+
+    function modifyPlayerStats(
+        uint256 _tokenId,
+        string memory _player,
+        address _sender
+    ) internal {
+        require(   
+            _isApprovedOrOwner(_sender, _tokenId),
+            "Function Caller must be the Token owner"
         );
+        playerInfo[_tokenId] = _player;
+    }
+
+    modifier whitelisted(
+        address _sender,
+        uint256 _tokenId,
+        string memory _player,
+        bytes memory _signature
+    ) {
+        require(
+            verify(_sender, _tokenId, _player, _signature) == true,
+            "No permission granted"
+        );
+        _;
+    }
+    function verify(
+        address _to,
+        uint256 _tokenId,
+        string memory _player,
+        bytes memory signature
+    ) internal view returns (bool) {
+        bytes32 messageHash = getMessageHash(_to, _tokenId, _player);
+        return recoverSigner(messageHash, signature) == owner();
+    }
+    function getMessageHash(
+        address _to,
+        uint256 _tokenId,
+        string memory _player
+    ) internal pure returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked(
+                    _tokenId,
+                    _to,
+                    _player
+                )
+            );
+    }
+
+    function recoverSigner(
+        bytes32 _ethSignedMessageHash,
+        bytes memory _signature
+    ) internal pure returns (address) {
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        (bytes32 _r, bytes32 _s, uint8 _v) = splitSignature(_signature);
+        bytes32 prefixedHashMessage = keccak256(
+            abi.encodePacked(prefix, _ethSignedMessageHash)
+        );
+        address signer = ecrecover(prefixedHashMessage, _v, _r, _s);
+        return signer;
+    }
+
+    function splitSignature(bytes memory sig)
+        internal
+        pure
+        returns (
+            bytes32 r,
+            bytes32 s,
+            uint8 v
+        )
+    {
+        require(sig.length == 65, "invalid signature length");
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+        return (r, s, v);
     }
 }
